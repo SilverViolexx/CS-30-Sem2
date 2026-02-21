a729b397594f25666da429de1723159beb6da192
Amanda <Amanda@10.0.0.140>
2026-02-20 19:26:35 -0700

    7:26

---
+++ Chapter10/src/Skillbuilder/MetricConversion.java
@@ -0,0 +1,150 @@
+package Skillbuilder;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.ListCellRenderer;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class MetricConversion 
+{
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					MetricConversion window = new MetricConversion();
+					window.frame.setVisible(true);
+				} catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MetricConversion() 
+	{
+		initialize();
+	}
+	
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.text);
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		//Prompt
+		JLabel prompt = new JLabel("Select A Conversion Type");
+		prompt.setBounds(32, 78, 184, 14);
+		prompt.setFont(new Font("Serif", Font.BOLD, 15));
+		prompt.setForeground(new Color(64, 0, 128));
+		panel.add(prompt);
+		
+		JLabel title = new JLabel("Metric Coversion");
+		title.setBounds(32, 11, 209, 36);
+		title.setFont(new Font("Serif", Font.BOLD, 25));
+		title.setForeground(new Color(64, 0, 128));
+		panel.add(title);
+		
+		//Where conversion is displayed
+		JLabel ans = new JLabel("");
+		ans.setFont(new Font("Serif", Font.BOLD, 15));
+		ans.setForeground(new Color(64, 0, 128));
+		ans.setBounds(32, 143, 249, 20);
+		panel.add(ans);
+		
+		//Provides options
+		JComboBox con = new JComboBox();
+		con.setBounds(32, 103, 184, 29);
+		con.setBackground(new Color(249, 242, 255));
+		con.setForeground(new Color(64, 0, 128));
+		con.setFont(new Font("Serif", Font.PLAIN, 14));
+		con.setModel(new DefaultComboBoxModel(new String[] {"Inches To Centimeters", "Feet To Meters", "Gallons To Meters", "Pounds To Kilograms"}));
+		panel.add(con);
+		
+		/*
+		con.setRenderer(new DefaultListCellRenderer()
+		{
+			public Component getListCellRendererComponent(JList<?> list, E value,  int index, boolean isSelected, boolean cellHasFocus) 
+			{ 
+				super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				if (isSelected) 
+				{
+					list.setBackground(Color.white);
+					list.setForeground(new Color(64, 0, 128));
+				}
+				else 
+				{
+					list.setForeground(new Color(64, 0, 128));
+					list.setBackground(new Color(249, 242, 255));
+				}        
+			}
+		}
+		
+		*/
+		
+		
+		//Displays conversion when clicked
+		con.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				if(con.getSelectedItem().equals("Inches To Centimeters"))
+				{
+					ans.setText("1 inch = 2.54 centimeters");
+				}
+				else if(con.getSelectedItem().equals("Feet To Meters"))
+				{
+					ans.setText("1 foot = 0.3048 meters");
+				}
+				else if(con.getSelectedItem().equals("Gallons To Meters"))
+				{
+					ans.setText("1 gallon = 4.5461 litres");
+				}
+				else
+				{
+					ans.setText("1 pound = 0.4536 kilograms");
+				}
+			}
+		});
+			
+		
+	}
+}
---
+++ Chapter10/src/Skillbuilder/SemesterAvg.java
@@ -0,0 +1,137 @@
+package Skillbuilder;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+
+public class SemesterAvg {
+
+	private JFrame frame;
+	private JTextField grade1;
+	private JTextField grade2;
+	private JTextField grade3;
+	private JTextField average;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SemesterAvg window = new SemesterAvg();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SemesterAvg() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setToolTipText("");
+		panel.setForeground(new Color(64, 0, 128));
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		//Prompts to input grade
+		JLabel gradePrompt = new JLabel("Enter The First Grade:");
+		gradePrompt.setFont(new Font("Serif", Font.BOLD, 15));
+		gradePrompt.setForeground(new Color(64, 0, 128));
+		gradePrompt.setBounds(21, 29, 165, 20);
+		panel.add(gradePrompt);
+		
+		JLabel gradePrompt2 = new JLabel("Enter The Second Grade:");
+		gradePrompt2.setForeground(new Color(64, 0, 128));
+		gradePrompt2.setFont(new Font("Serif", Font.BOLD, 15));
+		gradePrompt2.setBounds(21, 82, 178, 20);
+		panel.add(gradePrompt2);
+		
+		JLabel gradePrompt3 = new JLabel("Enter The Third Grade:");
+		gradePrompt3.setForeground(new Color(64, 0, 128));
+		gradePrompt3.setFont(new Font("Serif", Font.BOLD, 15));
+		gradePrompt3.setBounds(21, 138, 165, 20);
+		panel.add(gradePrompt3);
+		
+		//Where grades are inputed
+		grade1 = new JTextField();
+		grade1.setForeground(new Color(64, 0, 128));
+		grade1.setFont(new Font("Serif", Font.BOLD, 15));
+		grade1.setBounds(236, 27, 165, 28);
+		panel.add(grade1);
+		grade1.setColumns(10);
+		
+		grade2 = new JTextField();
+		grade2.setForeground(new Color(64, 0, 128));
+		grade2.setFont(new Font("Serif", Font.BOLD, 15));
+		grade2.setColumns(10);
+		grade2.setBounds(236, 84, 165, 28);
+		panel.add(grade2);
+		
+		grade3 = new JTextField();
+		grade3.setForeground(new Color(64, 0, 128));
+		grade3.setFont(new Font("Serif", Font.BOLD, 15));
+		grade3.setColumns(10);
+		grade3.setBounds(236, 140, 165, 28);
+		panel.add(grade3);
+		
+		//Where average grade is displayed
+		average = new JTextField();
+		average.setForeground(new Color(64, 0, 128));
+		average.setFont(new Font("Serif", Font.BOLD, 15));
+		average.setColumns(10);
+		average.setBounds(191, 196, 210, 33);
+		panel.add(average);
+		
+		//Submit button to display average
+		JButton submit = new JButton("Average");
+		submit.setBackground(new Color(245, 236, 255));
+		submit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String g1 = grade1.getText();
+				String g2 = grade2.getText();
+				String g3 = grade3.getText();
+				
+				//Calculates average
+				double avg = (Double.parseDouble(g1) + Double.parseDouble(g2) + Double.parseDouble(g3)) / 3;
+				
+				//Rounds to 2 decimal places
+				DecimalFormat df = new DecimalFormat("#0.00");
+
+				//Displays average
+				average.setText(" Your average is: " + df.format(avg));
+				
+			}
+		});
+		submit.setForeground(new Color(64, 0, 128));
+		submit.setFont(new Font("Serif", Font.BOLD, 15));
+		submit.setBounds(21, 196, 156, 33);
+		panel.add(submit);
+		
+	}
+}
