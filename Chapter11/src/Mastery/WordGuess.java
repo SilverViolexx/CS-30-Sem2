affdad9a178fc6a01e4aeb3a5a694cea5166d8f7
38207518 <38207518@L815WD220033.edu.cbe.ab.ca>
2026-03-12 15:37:21 -0600

    3:37

--- Chapter11/src/Mastery/WordGuess.java
+++ Chapter11/src/Mastery/WordGuess.java
@@ -1,3 +1,19 @@
+/*
+Program: WordGuess.java          Last Date of this Revision: March 13, 2026
+
+Purpose: Create a WordGuessing Game application that uses a word from a file as the secret word. The file 
+should contain a list of words, with one word per line. The WordGuess application should determine 
+which word to use, by generating a random number that corresponds to one of the words in the file.
+
+Author: Amanda Ly
+School: CHHS
+Course: Computer Programming 30
+ 
+*/
+
+
+
+
 package Mastery;
 
 import java.io.BufferedReader;
@@ -12,6 +28,7 @@ public class WordGuess {
 	
 	public static void main (String[] args) {
 		
+		//Loop sentinel - continues loop as long as input != sentinel
 		final String FLAG = "!";
 
 		//Create a File object with the name of your file is the parameter
@@ -31,8 +48,8 @@ public class WordGuess {
 		String secretWord = "";
 		String wordSoFar = "", updatedWord = "";
 		String letterGuess, wordGuess = "";
-		int numGuesses = 0;	
-		String word;
+		int numGuesses = 0;
+		//String word;
 		//char[] letters;
 
 		/* select secret word */
@@ -42,22 +59,35 @@ public class WordGuess {
                 //initialize the BufferedReader object to the name of the file reader as a parameter
                 readF = new BufferedReader(reads);
 				//Get the number of words in the file using readFile
-                while ((word = readF.readLine()) != null) {
+                while ((readF.readLine()) != null) {
                 	numWords ++;
                 }               		
-                		
+                System.out.println(numWords);	
+                
+                //Need to close and reopen it to read file again
+                readF.close();
+                reads.close();
+                
+                reads = new FileReader(textFile);           
+                readF = new BufferedReader(reads);                
+                
                 //update the word to guess to the random object and number of words read plus one
                 wordToGuess = ranNum.nextInt(1, numWords + 1);
-                for (int i = 0; i < numWords; i++) {
-                	wordGuess = readF.readLine();
+                System.out.println(wordToGuess);
+                for (int i = 0; i < wordToGuess; i++) {
+                	
+                	secretWord = readF.readLine();
+                	secretWord = secretWord.toUpperCase();
+                	System.out.println(secretWord);	
                 }
                 
-				
                 //iterate through the word to guess slots
+                /*
                 char[] letters = new char[wordGuess.length()];
                 for (int i = 0; i < wordGuess.length(); i ++) {
                 	letters[i] = wordGuess.charAt(i);
                 }
+                */
                         //update the secret work from the lines read from the file
                
                 //close your BufferedReader object
@@ -78,39 +108,50 @@ public class WordGuess {
 
 
 		/* begin the game */
-		System.out.println("WordGuess game.\n");
+		System.out.println("WordGuess game.");
 
         //iterate through the secret word, and update the word so far variable to represent using dashes 
         //the length of the secret word
         //output the word so far using dashes
-		for (int i = 0; i < letters.length(); i ++) {
-			
-		}
+		for (int i = 0; i < secretWord.length(); i ++) {
 
+			wordSoFar += "-";
+		}
+		System.out.println(wordSoFar);
 
 
 		/* allow player to make guesses*/
 
 		do {
 			//?? prompt for a letter
-
+			System.out.print("Guess A Letter (" + FLAG + " To Guess The Entire Word): ");
+			letterGuess = input.next();
+			letterGuess = letterGuess.toUpperCase();
 
 			/* increment number of guesses */
+			numGuesses += 1;
+			System.out.println(numGuesses);
 			//?
 
 			/* player correctly guessed a letter--extract string in wordSoFar up to the letter
 			 * guessed and then append guessed letter to that string. Next, extract rest of
 			 * wordSoFar and append after the guessed letter
 			 */
-			
-             //???
-
-
+			//indexOf - returns index where the first occurrence of "letterGuess" in the String
+			//Runs code if letter guessed is in the word
+			if (secretWord.indexOf(letterGuess) >= 0) {
+				System.out.println("HELLO");
+				//Extracts characters from the start of the word to just before the correctly guessed letter - replaces dash with letter guessed
+				updatedWord = wordSoFar.substring(0, secretWord.indexOf(letterGuess));
+				updatedWord += letterGuess;
+				updatedWord += wordSoFar.substring(secretWord.indexOf(letterGuess) + 1, wordSoFar.length());
+				wordSoFar = updatedWord;
+			}
 
 
 
 			/* display guessed letter instead of dash */
-			    //??
+			System.out.println(wordSoFar);
 
 
 		} while (!letterGuess.equals(FLAG) && !wordSoFar.equals(secretWord));
@@ -119,7 +160,19 @@ public class WordGuess {
 
         
 		/* finish game and display message and number of guesses */
-		
+		if (letterGuess.equals(FLAG)) {
+			System.out.print("What Is Your Guess: ");
+			wordGuess = input.next();
+			wordGuess = wordGuess.toUpperCase();
+		}
+		if (wordGuess.equals(secretWord) || wordSoFar.equals(secretWord)) {
+			System.out.println("You Won!");
+		}
+		else {
+			System.out.println("Sorry. You Lose.");
+		}
+		System.out.println("The Secret Word Is: " + secretWord);
+		System.out.println("You Made " + numGuesses + " guesses.");
         //??
 		
 		
