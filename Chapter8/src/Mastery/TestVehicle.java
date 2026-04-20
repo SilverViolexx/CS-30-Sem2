e678a1bdb5bf45cb259aeb215b97130771a82a48
38207518 <38207518@L815WD220033.edu.cbe.ab.ca>
2026-04-20 13:31:23 -0600

    1:31

--- Chapter8/src/Mastery/TestVehicle.java
+++ Chapter8/src/Mastery/TestVehicle.java
@@ -1,3 +1,16 @@
+/*
+Program: TestVehicle.java          Last Date of this Revision: April 20, 2026
+
+Create a Vehicle class that is an abstract class defining the general details and actions associated with 
+a vehicle. Create Car, Truck, and Minivan classes that inherit the Vehicle class. The Car, Truck, and 
+Minivan classes should include additional members specific to the type of vehicle being represented. 
+Create client code to test the classes.
+
+Author: Amanda Ly
+School: CHHS
+Course: Computer Programming 30
+
+*/
 package Mastery;
 
 import java.util.ArrayList;
@@ -14,15 +27,14 @@ public class TestVehicle {
 		ArrayList<Vehicle> truckList = new ArrayList<>();
 		ArrayList<Vehicle> vanList = new ArrayList<>();
 		String[] actionList = {"a", "v", "q"};
-		String[] viewList = {"c", "t", "m"};
 		String prompt = "(A)dd Vehicle || (V)iew Catalogue || (Q)uit";		
 		
+		//Display program name
 		System.out.println("VEHICLE CATALOGUE");
 		
+		//do-while loop
 		do {
-			
-			
-			
+				
 			System.out.println("\n" + prompt);
 			action = input.nextLine();
 		
@@ -45,61 +57,47 @@ public class TestVehicle {
 				System.out.println("View: (C)ars || (T)rucks || (M)inivan");
 				view = input.nextLine();
 				//Displays information of each object in arraylist
-				if (view.equalsIgnoreCase("c")) {			
-					if(carList.isEmpty() == true) {
+				if (view.equalsIgnoreCase("c")) {	
+					if(carList.isEmpty()) {
 						System.out.println("Car Catalogue Is Empty");
 					}
 					else {
-						for (int i = 0; i < carList.size(); i++) {
-							if(carList.get(i) == null) {
-								System.out.println("Car Information Missing." + "\n");
-							}
-							else{
-								System.out.println(carList.get(i) + "\n"); 
-							}
+						for (int i = 0; i < carList.size(); i++) {							
+							System.out.println(carList.get(i) + "\n"); 
 						}					
 					}
+					
 				}
 				else if (view.equalsIgnoreCase("t")) {				
-					if(truckList.isEmpty()  == true) {
+					if(truckList.isEmpty()) {
 						System.out.println("Truck Catalogue Is Empty");
 					}
 					else {
 						for (int i = 0; i < truckList.size(); i++) {
-							if(truckList.get(i) == null) {
-								System.out.println("Truck Information Missing." + "\n");
-							}
-							else{
-								System.out.println(truckList.get(i) + "\n"); 
-							}
+							System.out.println(truckList.get(i) + "\n"); 						
 						}						
 					}
 				}
 				else if (view.equalsIgnoreCase("m")) {	
-					if(vanList.isEmpty()  == true) {
+					if(vanList.isEmpty()) {
 						System.out.println("Minivan Catalogue Is Empty");
 					}
 					else {
 						for (int i = 0; i < vanList.size(); i++) {
-							if(vanList.get(i) == null) {
-								System.out.println("Minivan Information Missing." + "\n");
-							}
-							else{
-								System.out.println(vanList.get(i) + "\n"); 
-							}
+							System.out.println(vanList.get(i) + "\n"); 
 						}						
 					}
 				}
-				/*
-				System.out.println(prompt);
-				action = input.nextLine();
-				*/
+				else {
+					System.out.println("Invalid option.");
+				}
+
 			}
-			
-			while (!Arrays.asList(actionList).contains(action)) { //|| !Arrays.asList(viewList).contains(view)
+
+			while (!Arrays.asList(actionList).contains(action)) {
 				System.out.println("Invalid option.");
+				System.out.println("HELLO");
 				break;
-				//action = input.nextLine();
 			}
 		} while (!action.equalsIgnoreCase("q"));		
 	}
@@ -111,36 +109,46 @@ public class TestVehicle {
 		String action, type, make, colour, transmission, door;
 		int seatNum;
 		double cargoV, ctyEcon, hwyEcon, towCap;
+		String[] typeList = {"c", "t", "m"};
 		Scanner input = new Scanner(System.in);	
 		String typePrompt = "Type Of Vehicle: (C)ar || (T)ruck || (M)inivan";
-		
-		
-		
 			
 		System.out.print("Make & Model Of Vehicle: ");
 		make = input.nextLine();
+		
 		System.out.print("Colour Of Vehicle: ");
 		colour = input.nextLine();
+		
 		System.out.print("Seat Capacity: ");
 		seatNum = input.nextInt();
+		input.nextLine();
+		
 		System.out.print("Cargo Volume(m²): ");
 		cargoV = input.nextDouble();
+		input.nextLine();
+		
 		System.out.print("City Fuel Economy(km/L): ");
 		ctyEcon = input.nextDouble();
+		input.nextLine();
+		
 		System.out.print("Highway Fuel Economy(km/L): ");
 		hwyEcon = input.nextDouble();
+		input.nextLine();		
 		
-		//input.nextLine(); 
-		
-			
 		System.out.println(typePrompt);
-		type = input.next();
-			
-		if (type.equalsIgnoreCase("c")) {		
-			System.out.print("Transmission Type: ");
-			input.nextLine(); 
-			transmission = input.nextLine();
+		type = input.nextLine();
+		type = type.toLowerCase();
+		
+		while(!Arrays.asList(typeList).contains(type)) {
+			System.out.println("Invalid option.");
 
+			System.out.println(typePrompt);
+			type = input.nextLine();
+		}
+		
+		if (type.equalsIgnoreCase("c")) {		
+			System.out.print("Transmission Type(Auto/Manual): ");
+			transmission = input.nextLine();
 			//Returns object with data inputted
 			return (new Car(seatNum, cargoV, ctyEcon, hwyEcon, make, colour, transmission));
 		}
@@ -159,4 +167,5 @@ public class TestVehicle {
 		
 		return null;		
 	}
+	
 }
